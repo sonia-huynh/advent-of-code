15969214d7f629a3e6574e0adbf0356a6fa47319
Sonia Huynh <soniahuynhkl@gmail.com>
2024-06-22 23:29:28 +1200

    complete aoc2022 day4 pt1

---
+++ 2022/day-4/part1.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+
+// const input = fs.readFileSync("./test.txt", "utf-8").split("\n");
+const input = fs.readFileSync("./input.txt", "utf-8").split("\n");
+
+function campCleanup(input) {
+  //check how many pairs include the other fully
+  let count = 0;
+
+  //loop through the input to creat pair one set and pair two set
+  for (let pairs of input) {
+    const [first, second] = pairs.split(",");
+    const [firstStart, firstEnd] = first.split("-").map(Number);
+    const [secondStart, secondEnd] = second.split("-").map(Number);
+
+    //compare the start and end digits of each pair set and increase the count by 1 if it is true
+    if (firstStart >= secondStart && firstEnd <= secondEnd) {
+      count++;
+    } else if (secondStart >= firstStart && secondEnd <= firstEnd) {
+      count++;
+    }
+  }
+  console.log(count);
+  return count;
+}
+
+campCleanup(input);
